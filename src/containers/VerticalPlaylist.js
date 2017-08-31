cc31e18e0ee92bbac814899e864f3156aa5a0724
Daniel Lochrie <dclochri@gmail.com>
2017-08-31 13:41:52 -0500

    Added a variant that demonstrates a side-playlist.

--- src/containers/VerticalPlaylist.js
+++ src/containers/VerticalPlaylist.js
@@ -5,15 +5,20 @@ import { embedSourcePath, thumbPathLg, webPromoURL } from '../common/constants';
 
 import './VerticalPlaylist.css';
 
+const startIndex = 0;
+const endIndex = 3;
+
 class VerticalPlaylist extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      sliders: [],
+      videos: [],
       currentPage: [],
       currentVideo: {},
     };
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -21,17 +26,41 @@ class VerticalPlaylist extends Component {
         .then(response => response.json())
         .then((responseJson) => {
           this.setState({
-            sliders: responseJson.items,
-            currentVideo: responseJson.items[0],
-            currentPage: responseJson.items.slice(1, 4),
+            videos: responseJson.items,
+            currentVideo: 0,
+            currentPage: responseJson.items.slice(startIndex, endIndex),
           });
         });
   }
 
+  getPage() {
+    const { currentVideo, videos } = this.state;
+    const newIndex = videos.length === currentVideo + 1 ? 0 : currentVideo + 1;
+    const currentPage = videos.slice(newIndex + startIndex, newIndex + endIndex);
+
+    let cursor = 1;
+    while (currentPage.length < 3) {
+      currentPage.push(videos[cursor]);
+      cursor += 1;
+    }
+
+    this.setState({
+      currentVideo: newIndex,
+      currentPage,
+    });
+  }
+
+  handleClick() {
+    this.getPage();
+  }
+
   render() {
     const items = this.state.currentPage;
-    const currentVideo = this.state.currentVideo;
-    const src = `${embedSourcePath}${currentVideo.id}`;
+    const currentVideo = this.state.videos[this.state.currentVideo];
+    let src = '';
+    if (currentVideo) {
+      src = `${embedSourcePath}${currentVideo.id}`;
+    }
 
     return (
       <Row className="verticalPlaylist">
@@ -46,14 +75,21 @@ class VerticalPlaylist extends Component {
         <Col sm="3" xs="12">
           <Row>
             {items && items.length ?
-              items.map(item => (
+              items.map((item, index) => (
                 <Col xs="12" key={item.id}>
-                  <img
-                    src={`${thumbPathLg}${item.id}`}
-                    alt={item.name}
-                    className="playlist-img"
-                  />
-                  <h5>{item.name}</h5>
+                  <div
+                    className={index === 0 ? 'verticalPlaylist__item--current' : 'verticalPlaylist__item'}
+                    onClick={this.handleClick}
+                    role="presentation"
+                  >
+                    <img
+                      src={`${thumbPathLg}${item.id}`}
+                      alt={item.name}
+                      className="playlist-img"
+                    />
+                    {index === 0 ? (<h4>Now Playing</h4>) : ''}
+                    <h5>{item.name}</h5>
+                  </div>
                 </Col>
               ))
               : null}
